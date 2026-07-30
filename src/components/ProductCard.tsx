2eed04dfae34714d97bb8f41c273cc91be4f6f56
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-07-30 20:07:27 +0200

    feat(ProductCard): remove unused import

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,22 +1,15 @@
-import { useState } from 'react';
 import type { Product } from '../types/product';
 
 export interface ProductCardProps {
     product: Product
 }
 
-        // "id": "1",
-        // "title": "Lápiz",
-        // "price": 2,
-        // "description":"Super lápiz",
-        // "image":"src",
-        // "category":"Papelería"
 export const ProductCard = ({product: {title, description, image}}: ProductCardProps ) => {
     return (
         <li>
             <h2>{title}</h2>
             <p>{description}</p>
-            <img src={image} alt="" />
+            <img src={image} alt={description} />
         </li>
     );
-} 
+}
