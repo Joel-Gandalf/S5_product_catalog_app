b115ea797569ea053d1e508752a2f0dcc0264ec3
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-07-30 10:56:40 +0200

    feat(routing):  integrate dynamic routes and programmatic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+import { Routes, Route } from 'react-router'
+import { Layout } from './components/Layout'
+import { Home } from './pages/Home'
+import { ProductList } from './pages/BookList'
+import { ProductDetails } from './pages/BookDetails'
+import { NotFound } from './pages/NotFound'
 
 export const App = () => {
 
   return (
-    <>
-
-    </>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:productId" element={<ProductDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
   )
 }
